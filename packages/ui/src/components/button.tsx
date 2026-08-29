04bf18721c37b8610b91fa33c41c6bf010e86a5d
PzHown <1263097055@qq.com>
2026-08-29 11:26:02 +0800

    fix(ui): normalize button geometry

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -12,7 +12,7 @@ import {
 import { cn } from '../lib/utils'
 
 const buttonVariants = cva(
-  'pzhown-ui pzhown-button tw:inline-flex tw:shrink-0 tw:items-center tw:justify-center tw:gap-1.5 tw:whitespace-nowrap tw:border tw:border-transparent tw:text-sm tw:font-medium tw:outline-none tw:select-none tw:disabled:pointer-events-none tw:disabled:opacity-50 tw:[&_svg]:pointer-events-none tw:[&_svg]:shrink-0 tw:[&_svg:not([class*=size-])]:size-4',
+  'pzhown-ui pzhown-button tw:inline-flex tw:shrink-0 tw:items-center tw:justify-center tw:gap-2 tw:whitespace-nowrap tw:border tw:border-transparent tw:text-sm tw:font-medium tw:outline-none tw:select-none tw:disabled:pointer-events-none tw:disabled:opacity-50 tw:[&_svg]:pointer-events-none tw:[&_svg]:shrink-0 tw:[&_svg:not([class*=size-])]:size-4',
   {
     variants: {
       variant: {
@@ -24,14 +24,14 @@ const buttonVariants = cva(
         link: '',
       },
       size: {
-        default: 'tw:h-9 tw:px-3.5',
-        xs: 'tw:h-7 tw:px-2.5 tw:text-xs',
-        sm: 'tw:h-8 tw:px-3 tw:text-[0.8rem]',
-        lg: 'tw:h-10 tw:px-4',
-        icon: 'tw:size-9',
-        'icon-xs': 'tw:size-7',
-        'icon-sm': 'tw:size-8',
-        'icon-lg': 'tw:size-10',
+        default: 'tw:h-11 tw:px-4',
+        xs: 'tw:h-9 tw:px-3 tw:text-xs',
+        sm: 'tw:h-10 tw:px-3 tw:text-[0.8rem]',
+        lg: 'tw:h-12 tw:px-5',
+        icon: 'tw:size-11',
+        'icon-xs': 'tw:size-9',
+        'icon-sm': 'tw:size-10',
+        'icon-lg': 'tw:size-12',
       },
     },
     defaultVariants: {
