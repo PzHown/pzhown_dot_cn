77de4aa939b47e9a60563edd4697f72978c4397f
PzHown <1263097055@qq.com>
2026-08-31 09:42:43 +0800

    chore(ui): export liquid glass source ref type

--- packages/ui/src/react.ts
+++ packages/ui/src/react.ts
@@ -2,7 +2,7 @@ import ProgressiveBlur from './ProgressiveBlur'
 
 export { ProgressiveBlur }
 export { ThemeProvider, useTheme, type ThemeMode, type ResolvedThemeMode } from './theme-provider'
-export { LiquidGlassProvider, useLiquidGlass, useLiquidGlassEnabled, type LiquidGlassContextValue, type LiquidGlassProviderProps } from './liquid-glass-provider'
+export { LiquidGlassProvider, useLiquidGlass, useLiquidGlassEnabled, type LiquidGlassContextValue, type LiquidGlassProviderProps, type LiquidGlassSourceRef } from './liquid-glass-provider'
 export * from './components/index'
 
 export default ProgressiveBlur
