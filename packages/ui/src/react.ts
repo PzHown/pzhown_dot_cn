ede4fad8556285a92cb980dca171398d7c8801a0
PzHown <1263097055@qq.com>
2026-08-31 12:36:42 +0800

    feat(ui): export PallavAg host stage

--- packages/ui/src/react.ts
+++ packages/ui/src/react.ts
@@ -4,15 +4,19 @@ export { ProgressiveBlur }
 export { ThemeProvider, useTheme, type ThemeMode, type ResolvedThemeMode } from './theme-provider'
 export {
   LiquidGlassProvider,
+  LiquidGlassStage,
   LiquidGlassViewport,
   useLiquidGlass,
   useLiquidGlassEnabled,
   type ExternalLiquidGlassOptions,
+  type LiquidGlassContainerRef,
   type LiquidGlassContextValue,
+  type LiquidGlassDefsHostRef,
   type LiquidGlassOpticalOverrides,
   type LiquidGlassPortalRef,
   type LiquidGlassProviderProps,
   type LiquidGlassSourceRef,
+  type LiquidGlassStageProps,
   type LiquidGlassViewportProps,
 } from './liquid-glass-provider'
 export * from './components/index'
